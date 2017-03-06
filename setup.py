f783d931c7d85f7d60f7775bc75f7bc9427c7f73
Eugene Pokidov <pokidovea@gmail.com>
2017-03-06 22:53:15 +0300

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='immobilus',
-    version='0.2.1',
+    version='0.2.2',
     description='Say `Immobilus!` to freeze your tests',
     author='Eugene Pokidov',
     author_email='pokidovea@gmail.com',
