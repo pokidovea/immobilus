6aaae449a7ee2512f93275cf8fb9144ab838b9b4
Eugene Pokidov <pokidovea@gmail.com>
2017-10-15 01:16:56 +0300

    fix mktime

--- tests/test_mktime.py
+++ tests/test_mktime.py
@@ -8,16 +8,17 @@ import pytz
 
 def test_mktime_without_timezone():
     with immobilus('1970-01-01 00:00:00'):
-        timetuple = datetime.now().timetuple()
         timestamp = _datetime_to_utc_timestamp(datetime.now())
 
+        timetuple = datetime.now().timetuple()
         mktime = time.mktime(timetuple)
+
         assert mktime == timestamp
 
 
 def test_mktime_gets_timezone_from_timetuple():
     timezone = pytz.timezone('US/Eastern')
-    dt = datetime(1970, 1, 1, 0, 0, tzinfo=timezone)
+    dt = timezone.localize(datetime(1970, 1, 1, 0, 0))
 
     with immobilus(dt):
         timetuple = datetime.now().timetuple()
