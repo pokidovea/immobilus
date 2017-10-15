d8d77d4dd98d9287be8a98f0024e5f458bef2b66
Day Barr <github.com@daybarr.com>
2017-10-16 00:23:32 +0100

    Tidy test - reuse timestamp

--- tests/test_time.py
+++ tests/test_time.py
@@ -8,11 +8,12 @@ from time import time
 def test_time_function():
 
     dt = datetime(1970, 1, 1)
-    assert _datetime_to_utc_timestamp(dt) == 0.0
-    assert type(_datetime_to_utc_timestamp(dt)) is float
-    assert time() != _datetime_to_utc_timestamp(dt)
+    timestamp = _datetime_to_utc_timestamp(dt)
+    assert timestamp == 0.0
+    assert type(timestamp) is float
+    assert time() != timestamp
 
     with immobilus(dt):
-        assert time() == _datetime_to_utc_timestamp(dt)
+        assert time() == timestamp
 
-    assert time() != _datetime_to_utc_timestamp(dt)
+    assert time() != timestamp
