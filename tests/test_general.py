b9f786f85fda501edc5d70d81029d6d7caa4f0de
Éloi Rivard <azmeuk@gmail.com>
2019-08-21 19:03:38 +0200

    Implemented tick method

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,7 +1,7 @@
 from immobilus.logic import immobilus
 
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def test_transmutations():
@@ -19,3 +19,12 @@ def test_transmutations():
         fromtimestamp = datetime.fromtimestamp(mktime)
 
         assert now.replace(microsecond=0) == fromtimestamp
+
+
+def test_tick():
+    with immobilus('2019-08-21 12:00:00') as dt:
+        assert datetime(2019, 8, 21, 12, 0, 0) == datetime.now()
+        dt.tick()
+        assert datetime(2019, 8, 21, 12, 0, 1) == datetime.now()
+        dt.tick(timedelta(seconds=10))
+        assert datetime(2019, 8, 21, 12, 0, 11) == datetime.now()
